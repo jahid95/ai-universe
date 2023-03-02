0dddd140a1c8e845fc96074bfccfc7eadfdcea63
Anwar <anwarjahid449@gmail.com>
2023-03-02 21:05:00 +0600

    set dynamic card

--- js/app.js
+++ js/app.js
@@ -21,11 +21,12 @@ const displayData = items =>{
     const cardContainer = document.getElementById('card-container');
     
     items.forEach(item => {
+        const {image} = item;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
