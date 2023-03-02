3a6aaec2c3ae977d09945782859f2c3260a792f2
Anwar <anwarjahid449@gmail.com>
2023-03-03 01:11:01 +0600

    ternary operator use for null value

--- js/app.js
+++ js/app.js
@@ -44,9 +44,9 @@ const displayData = (items, dataLimit) => {
         <div class="card-body">
           <h5 class="card-title">Features</h5>
           <ol>
-          <li>${features[0]}<li>
-          <li>${features[1]}<li>
-          <li>${features[2]}<li>        
+          <li>${features[0]}</li>
+          <li>${features[1]}</li>
+          <li>${features[2]}</li>        
           </ol>
         </div>
         <div class="card-footer d-flex justify-content-between align-items-center">
@@ -79,6 +79,7 @@ const itemDetails = id => {
 }
 
 const displayItemDetails = details => {
+    const {image_link,input_output_examples,description,pricing,accuracy} = details;
     console.log(details);
     const modalRight = document.getElementById('modal-right');
     const modalLeft = document.getElementById('modal-left');
@@ -87,9 +88,26 @@ const displayItemDetails = details => {
     const divRight = document.createElement('div');
     divRight.classList.add('col');
     divRight.innerHTML = `
-    <div class="card p-4">
-    <h2 class="card-title">${details.description}</h2>
+    <div class="card p-4 bg-danger-subtle">
+    <h2 class="card-title">${description}</h2>
     <div class="card-body">
+    <div class="d-flex justify-content-around p-3 text-danger">
+    <h5 class="p-3 m-2 bg-white rounded">
+    <span>${pricing? pricing[0].price : 'Free of Cost/'}<span>
+    </br>
+    <span>${pricing? pricing[0].plan : 'Basic'}<span>
+    </h5>
+    <h5 class="p-3 m-2 bg-white rounded">
+    <span>${pricing? pricing[1].price : 'Free Of Cost'}<span>
+    </br>
+    <span>${pricing? pricing[1].plan : '/Pro'}<span>
+    </h5>
+    <h5 class="p-3 m-2 bg-white rounded">
+    <span>${pricing? pricing[2].price : 'Free of Cost'}<span>
+    </br>
+    <span>${pricing? pricing[2].plan : '/ Enterprise'}<span>
+    </h5>
+    </div>
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a
             natural lead-in to additional content. This content is a little bit longer.
@@ -101,14 +119,13 @@ const displayItemDetails = details => {
     const divLeft = document.createElement('div');
     divLeft.classList.add('col');
     divLeft.innerHTML =`
-    <div class="card p-4">
-    <img src="${details.image_link[1]}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a
-            natural lead-in to additional content. This content is a little bit
-            longer.</p>
+    <div class="card p-4 h-100">
+    <img src="${image_link[0]}" class="card-img-top img-fluid" alt="...">
+    <div class="card-body text-center">
+        <h5 class="card-title">${input_output_examples? input_output_examples[0].input : 'No! Not Yet! Take a break!!!'}</h5>
+        <p class="card-text">${input_output_examples? input_output_examples[0].output.slice(0,140) : 'No! Not Yet! Take a break!!!'}</p>
     </div>
+    <h4 class="bg-danger text-white rounded position-absolute p-2 top-10 end-0"> ${accuracy.score? accuracy.score +' '+ 'accuracy ': ''}</h4>
     </div>
     `;
     modalLeft.appendChild(divLeft);
