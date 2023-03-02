ccef28ea2217196ea1fa6828c3bafc2a9cefe937
Anwar <anwarjahid449@gmail.com>
2023-03-02 22:28:00 +0600

    using bootstrap modal

--- js/app.js
+++ js/app.js
@@ -31,19 +31,31 @@ const displayData = (items, dataLimit) =>{
     }
     
     items.forEach(item => {
-        const {image} = item;
+        const {image, features, name, published_in} = item;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
         <img src="${image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h5 class="card-title">Features</h5>
+          <ol>
+          <li>${features[0]}<li>
+          <li>${features[1]}<li>
+          <li>${features[2]}<li>        
+          </ol>
         </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+        <div class="card-footer d-flex justify-content-between align-items-center">
+        <div>
+        <h5 class="card-title">${name}</h5>  
+        <div class="d-flex gx-5 aligns-content-center">
+        <i class="mt-1 fa-regular fa-calendar"></i>
+        <p class="mx-2">${published_in}</p>    
         </div>
+      </div>
+      <div>
+      <i class="bg-danger-subtle text-danger rounded-circle p-2 fa-solid fa-arrow-right"></i>
+      </div>
       </div>
         `;
         cardContainer.appendChild(div);
