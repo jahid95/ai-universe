f06f7fa5687619ba33ba40a8cdf34fa30cce0e95
Anwar <anwarjahid449@gmail.com>
2023-03-02 21:00:13 +0600

    toggle spinner added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,48 @@
+const toggleSpinner = isLoading =>{
+    const spinner = document.getElementById('spinner');
+    if(isLoading){
+        spinner.classList.remove('d-none');
+    }
+    else{
+        spinner.classList.add('d-none');
+    }    
+}
+
 const loadData =()=>{
     const url = 'https://openapi.programming-hero.com/api/ai/tools';
     fetch(url)
     .then(res => res.json())
-    .then(data => displayData(data.data))
+    .then(data => displayData(data.data.tools))
+    toggleSpinner(true);
 }
 
-const displayData = items =>{
+const displayData = items =>{    
     console.log(items);
+    const cardContainer = document.getElementById('card-container');
+    
+    items.forEach(item => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+        `;
+        cardContainer.appendChild(div);
+    })
+    toggleSpinner(false);
 }
+
+
 
 loadData();
+
+
+
